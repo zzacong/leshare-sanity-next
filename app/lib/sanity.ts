bd520937d790fb71f96439da382e998e1f2a2fc3
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-04-07 23:11:35 +1000

    use pnpm

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -7,7 +7,7 @@ import {
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import createImageUrlBuilder from '@sanity/image-url'
 
-export const sanityConfig: ClientConfig = {
+export const sanityConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '',
   apiVersion: '2021-10-21',
